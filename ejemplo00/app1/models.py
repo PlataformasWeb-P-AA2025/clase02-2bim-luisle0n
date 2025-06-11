41ce2841d34d405537f1b87ccca123b4b1820d47
luisle0n <lfleon9@utpl.edu.ec>
2025-06-11 08:53:07 -0500

    Creacion de una clase llamada ciudad

--- ejemplo00/app1/models.py
+++ ejemplo00/app1/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Estudiante (models.Model):
+    nombre =models.CharField(max_length=30)
+    apellido =models.CharField(max_length=30)
+    cedula =models.CharField(max_length=30)
+    edad =models.IntegerField()
+
+    def __str__(self):
+        return "%s - %s - %s - edad: %d" % (self.nombre, 
+                                            self.apellido,
+                                            self.cedula,
+                                            self.edad)
+
+class Ciudad (models.Model):
+    nombre=models.CharField(max_length=60)
+    poblacion=models.IntegerField()
+    def __str__(self):
+        return "%s - poblacion_: %d" % (self.nombre, 
+                                            self.poblacion)
